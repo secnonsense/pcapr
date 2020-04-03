13fea1e1238ce0d3a18a15da6ed2cc3797687614
secnonsense <scott@secnonsense.com>
2020-04-03 07:59:52 -0400

    adding protocols list

--- pcapr.py
+++ pcapr.py
@@ -26,18 +26,22 @@ def process_pcap(pcap):
 
     if pcap.haslayer(Ether):
         print "Ether Src: " + pcap[Ether].src + " - Ether Dst: " + pcap[Ether].dst + " - Ether Type: " + str(hex(pcap[Ether].type)),
+        protocols.append("Ether")
 
     if pcap.haslayer(IP):
         print "\r\nIP Src: " + pcap[IP].src + " - IP Dst: " + pcap[IP].dst + " - IP ID: " + str(pcap[IP].id) + " - TTL: " + str(pcap[IP].ttl)
+        protocols.append("IP")
 
     if pcap.haslayer(UDP):
         print "UDP - Source Port: " + str(pcap[UDP].sport) + "  Destination Port: " + str(pcap[UDP].dport),   
+        protocols.append("UDP")
 
     if pcap.haslayer(TCP):
         sequence_number = pcap[TCP].seq
         acknowledgement_number = pcap[TCP].ack
         timestamp = pcap[TCP].time
         payload_len += len(pcap[TCP].payload)
+        protocols.append("TCP")
         
         print "TCP - Source Port: " + str(pcap[TCP].sport) + "  Destination Port: " + str(pcap[TCP].dport)
         print "Response seq: " + str(sequence_number) + " ack: " + \
@@ -65,18 +69,21 @@ def process_pcap(pcap):
 
     if pcap.haslayer(HTTP):  
         print "\r\nHTTP - " + pcap[HTTP].method,
+        protocols.append("HTTP")
 
     if pcap.haslayer(ARP):   
         print "\r\nARP - "  + "Hardware Source: " + pcap[ARP].hwsrc + " -  Source Addr: " + pcap[ARP].psrc + " - op: " + str(pcap[ARP].op),
+        protocols.append("ARP")
 
     if pcap.haslayer(DNS):   
         print "\r\nDNS - "  + str(pcap[DNS].qd), str(pcap[DNS].an), str(pcap[DNS].ns), str(pcap[DNS].ar),
-    
+        protocols.append("DNS")
+
     if pcap.haslayer(TLS):   
         print "\r\nTLS - "  + "Type: " + str(pcap[TLS].type) + " -  Version: " + str(pcap[TLS].version),
         if "TLSApplicationData" not in  str(pcap[TLS].msg):
             print " -  Message: " + str(pcap[TLS].msg), 
-    
+        protocols.append("TLS")
     print "\r\n------------\r\n"    
 
 parser = argparse.ArgumentParser()
