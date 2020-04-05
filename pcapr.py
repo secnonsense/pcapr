27f57babdfc56543e310a80a2632ba35a7c4e3d4
secnonsense <scott@secnonsense.com>
2020-04-04 21:26:02 -0400

    building query logic

--- pcapr.py
+++ pcapr.py
@@ -7,7 +7,6 @@ load_layer("tls")
 load_layer("http")
 
 payload_len = 0
-protocols = []
 
 def process_pcap(pcap):
     FIN = 0x01
@@ -25,69 +24,80 @@ def process_pcap(pcap):
     global payload_len
 
     if pcap.haslayer(Ether):
-        print "Ether Src: " + pcap[Ether].src + " - Ether Dst: " + pcap[Ether].dst + " - Ether Type: " + str(hex(pcap[Ether].type)),
-        protocols.append("Ether")
+        if not args.query or "Ether" in args.query or (pcap.haslayer(DNS) and "DNS" in args.query):
+            print "\r\nEther Src: " + pcap[Ether].src + " - Ether Dst: " + pcap[Ether].dst + " - Ether Type: " + str(hex(pcap[Ether].type)),
+    
 
     if pcap.haslayer(IP):
-        print "\r\nIP Src: " + pcap[IP].src + " - IP Dst: " + pcap[IP].dst + " - IP ID: " + str(pcap[IP].id) + " - TTL: " + str(pcap[IP].ttl)
-        protocols.append("IP")
+        if not args.query or "IP" in args.query or (pcap.haslayer(DNS) and "DNS" in args.query):
+            print "\r\nIP Src: " + pcap[IP].src + " - IP Dst: " + pcap[IP].dst + " - IP ID: " + str(pcap[IP].id) + " - TTL: " + str(pcap[IP].ttl)
+        
 
     if pcap.haslayer(UDP):
-        print "UDP - Source Port: " + str(pcap[UDP].sport) + "  Destination Port: " + str(pcap[UDP].dport),   
-        protocols.append("UDP")
+        if not args.query or "UDP" in args.query or (pcap.haslayer(DNS) and "DNS" in args.query):
+            print "UDP - Source Port: " + str(pcap[UDP].sport) + "  Destination Port: " + str(pcap[UDP].dport),   
+        
 
     if pcap.haslayer(TCP):
         sequence_number = pcap[TCP].seq
         acknowledgement_number = pcap[TCP].ack
         timestamp = pcap[TCP].time
         payload_len += len(pcap[TCP].payload)
-        protocols.append("TCP")
+        if not args.query or "TCP" in args.query:
         
-        print "TCP - Source Port: " + str(pcap[TCP].sport) + "  Destination Port: " + str(pcap[TCP].dport)
-        print "Response seq: " + str(sequence_number) + " ack: " + \
-              str(acknowledgement_number) + " timestamp: " + str(timestamp) + " len: " + \
-              str(len(pcap[TCP].payload)) + " window: " + str(pcap[TCP].window) + " options: " + str(pcap[TCP].options)
-        F = pcap['TCP'].flags    
-        print "Flags: ",
-        if F & FIN:
-            print "FIN",
-        if F & SYN:
-            print "SYN",
-        if F & RST:
-            print "RST",
-        if F & ACK:
-            print "ACK",
-        if F & PSH:
-            print "PSH",
-        if F & URG:
-            print "URG",        
-        if F & ECE:
-            print "ECE",
-        if F & CWR:
-            print "CWR"
-         
+        
+            print "TCP - Source Port: " + str(pcap[TCP].sport) + "  Destination Port: " + str(pcap[TCP].dport)
+            print "Response seq: " + str(sequence_number) + " ack: " + \
+                str(acknowledgement_number) + " timestamp: " + str(timestamp) + " len: " + \
+                str(len(pcap[TCP].payload)) + " window: " + str(pcap[TCP].window) + " options: " + str(pcap[TCP].options)
+            F = pcap['TCP'].flags    
+            print "Flags: ",
+            if F & FIN:
+                print "FIN",
+            if F & SYN:
+                print "SYN",
+            if F & RST:
+                print "RST",
+            if F & ACK:
+                print "ACK",
+            if F & PSH:
+                print "PSH",
+            if F & URG:
+                print "URG",        
+            if F & ECE:
+                print "ECE",
+            if F & CWR:
+                print "CWR"
+                   
 
     if pcap.haslayer(HTTP):  
-        print "\r\nHTTP - " + pcap[HTTP].method,
-        protocols.append("HTTP")
+        if not args.query or "HTTP" in args.query:
+            print "\r\nHTTP - " + pcap[HTTP].method
+        
 
-    if pcap.haslayer(ARP):   
-        print "\r\nARP - "  + "Hardware Source: " + pcap[ARP].hwsrc + " -  Source Addr: " + pcap[ARP].psrc + " - op: " + str(pcap[ARP].op),
-        protocols.append("ARP")
+    if pcap.haslayer(ARP):  
+        if not args.query or "ARP" in args.query: 
+            print "\r\nARP - "  + "Hardware Source: " + pcap[ARP].hwsrc + " -  Source Addr: " + pcap[ARP].psrc + " - op: " + str(pcap[ARP].op)
+        
 
-    if pcap.haslayer(DNS):   
-        print "\r\nDNS - "  + str(pcap[DNS].qd), str(pcap[DNS].an), str(pcap[DNS].ns), str(pcap[DNS].ar),
-        protocols.append("DNS")
+    if pcap.haslayer(DNS):
+        if not args.query or "DNS" in args.query:   
+            print "\r\nDNS - "  + str(pcap[DNS].qd), str(pcap[DNS].an), str(pcap[DNS].ns), str(pcap[DNS].ar) + "\r\n"
+    
 
-    if pcap.haslayer(TLS):   
-        print "\r\nTLS - "  + "Type: " + str(pcap[TLS].type) + " -  Version: " + str(pcap[TLS].version),
-        if "TLSApplicationData" not in  str(pcap[TLS].msg):
-            print " -  Message: " + str(pcap[TLS].msg), 
-        protocols.append("TLS")
-    print "\r\n------------\r\n"    
+    if pcap.haslayer(TLS): 
+        if not args.query or "TLS" in args.query:  
+            print "\r\nTLS - "  + "Type: " + str(pcap[TLS].type) + " -  Version: " + str(pcap[TLS].version)
+            if "TLSApplicationData" not in  str(pcap[TLS].msg):
+                print " -  Message: " + str(pcap[TLS].msg)
+        
+    #if (not pcap.haslayer(HTTP) and not pcap.haslayer(ARP)) and pcap.haslayer(TCP):
+     #   print "\r\n"
+    #print "\r\n------------\r\n"    
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--read", help="read pcapfile",action="store", dest="pcap", required=True)
+parser.add_argument("-q", "--query", help="Query for protocols to print",action="store", dest="query")
 args = parser.parse_args()
 
 packets = rdpcap(args.pcap)
